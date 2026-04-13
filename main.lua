0a3757777fae09c1d01d83201d8d91a70bbdda5c
10-thousand-bees <haristongbanhos@gmail.com>
2026-04-13 20:36:19 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,27 +1,24 @@
-
 local MAPA_A = 10705063265 -- Hueco Mundo
 local MAPA_B = 137886107419750 -- Mission place
 local MAPA_C = 15998522552 -- Karakura
 
-
 local currentPlaceId = game.PlaceId
+
+print("Current PlaceId: " .. currentPlaceId)
 
 if currentPlaceId == MAPA_A then
     print("[Paradoxxing]: Hueco Mundo - Loaded!")
     loadstring(game:HttpGet("https://raw.githubusercontent.com/10-thousand-bees/paradoxxing/refs/heads/main/paradoxxing.lua"))()
-
+    
 elseif currentPlaceId == MAPA_B then
     print("[Paradoxxing]: Mission Place - Loaded!")
     loadstring(game:HttpGet("https://raw.githubusercontent.com/10-thousand-bees/paradoxxing/refs/heads/main/HMission.lua"))()
     
 elseif currentPlaceId == MAPA_C then
     print("[Paradoxxing]: Karakura - Loaded!")
-        
     loadstring(game:HttpGet("https://raw.githubusercontent.com/10-thousand-bees/paradoxxing/refs/heads/main/paradoxxing.lua"))()
     warn("i dont even know what this map is about, so im loading the main script")
         
 else
     warn("Unknown Game/World ")
 end
-
---pls dont make fun of my loader :(

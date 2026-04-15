5ffffe167f67cc71de3eb13469e8568491618387
10-thousand-bees <haristongbanhos@gmail.com>
2026-04-15 18:32:51 -0300

    Create offsets.lua
    offsets file added to the main script

---
+++ offsets.lua
@@ -0,0 +1,18 @@
+_G.OFFSETS_DATA = {
+    version = "version-26c90be22e0d4758",
+    Lighting = {
+        Ambient = 0xd8,
+        OutdoorAmbient = 0x108,
+        Brightness = 0x120,
+        FogEnd = 0x134,
+        FogStart = 0x138
+    },
+    Atmosphere = {
+        Density = 0xe8,
+        Haze = 0xf0,
+        Glare = 0xec
+    },
+    Humanoid = {
+        WalkSpeed = 0x1d4
+    }
+}
